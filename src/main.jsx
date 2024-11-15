3544128b28f4707044f6dd7e408b021c650544cd
adistar964 <adistar964@gmail.com>
2024-11-15 23:37:21 +0300

    changed smth

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,7 @@ const myRouter = createBrowserRouter([
   {path:"/blogs", element: <BlogListPage />},
   {path:"/blogs/:blogID", element: <BlogPage />},
   {path:"/projects/:projectsCategory", element: <ProjectsPage />},
-])
+], {basename:import.meta.env.BASE_URL})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
